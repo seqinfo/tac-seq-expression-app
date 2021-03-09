e05958103143ce5d30a9a5e030b2928d6f8b2f1a
allumik <alvinmeltsov@gmail.com>
2021-03-09 15:27:26 +0200

    Added scoring for the app, first working version

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This repository contains TAC-seq expression app for TAC-seq data analysis.
   * column `sample` - sample ID
   * column `locus` - locus ID
   * column `molecule_count` - molecule count
+
+* Target gene subset for modelling:
+  * column `locus` - only one column with header "locus" and a list of selected gene names. Have to be a subset of `target` -> `locus` values.
   
 * Target file with:
   * column `target` - values have to match with `locus` values in [TAC-seq data analysis](https://github.com/cchtEE/TAC-seq-data-analysis) output file
@@ -20,3 +23,11 @@ This repository contains TAC-seq expression app for TAC-seq data analysis.
 Normalized count table with:
 * column `sample` - sample ID
 * column for each `biomarker`
+
+Score table with:
+* column `sample` - sample ID
+* column `pre-rec` - sample score for the pre-receptivity
+* column `rec` - sample score for the receptivity
+* column `post-rec` - sample score for the post-receptivity
+* column `predcted group` - the predicted class of the sample
+* column `score` - general score to be used in the receptometer ranging from (pre=) 0 - (rec=) 100 - (post=) 200
--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
+library(foreach)
+library(glue)
 library(DT)
 library(recipes)
 library(heatmaply)
@@ -8,6 +10,7 @@ library(RColorBrewer)
 library(plotly)
 library(embed)
 
+#### UI
 
 ui <- dashboardPage(
   dashboardHeader(title = "TAC-seq expression app"),
@@ -16,6 +19,7 @@ ui <- dashboardPage(
       menuItem("Input", tabName = "input"),
       menuItem("Normalization", tabName = "normalization"),
       menuItem("Visualization", tabName = "visualization"),
+      menuItem("Scoring", tabName = "scoring"),
       tags$a(
         href = "https://github.com/seqinfo/tac-seq-expression-app", "GitHub",
         align = "center", style = "
@@ -78,22 +82,52 @@ ui <- dashboardPage(
             "READY HRT controls (400k reads)" = "data/controls/READY_HRT_controls_400k_reads.tsv",
             "READY HRT controls (all reads)" = "data/controls/READY_HRT_controls_all_reads.tsv",
             "READY HRT controls (read count)" = "data/controls/READY_HRT_controls_read_count.tsv",
-            "READY PCOS controls (400k reads)" = "data/controls/READY_PCOS_controls_400k_reads.tsv"
+            "READY PCOS controls (400k reads)" = "data/controls/READY_PCOS_controls_400k_reads.tsv",
+            "READY PCOS controls (filtered outliers)" = "data/controls/READY_PCOS_controls_400k_filtered.tsv"
           )
         ),
         fileInput("control_file", label = "", accept = "text"),
         h3("Controls"),
         dataTableOutput("controls"),
-        h3("Heatmap"),
-        plotlyOutput("heatmap", width = "auto", height = 900),
         h3("PCA"),
-        plotlyOutput("pca", width = "auto", height = 900),
+        plotlyOutput("pca", width = "auto", height = "auto"),
         h3("UMAP"),
-        plotlyOutput("umap", width = "auto", height = 900)
+        plotlyOutput("umap", width = "auto", height = "auto"),
+        h3("Heatmap"),
+        plotlyOutput("heatmap", width = "auto", height = "auto")
+      ),
+      tabItem(
+        tabName = "scoring",
+        h1("Scoring"),
+        p("Score and classify the input samples depending on the reference groups."),
+        selectInput(
+          "scoring_ref", label = "Choose reference for scoring:",
+          choices = list(
+            "Choose one" = "",
+            "READY PCOS controls (400k reads)" = "data/controls/READY_PCOS_controls_400k_reads.tsv",
+            "READY PCOS controls (filtered outliers)" = "data/controls/READY_PCOS_controls_400k_filtered.tsv"
+          )
+        ),
+        selectInput(
+          "gene_subset", label = "Choose the gene subset to apply:",
+          choices = list(
+            "Choose one" = "",
+            "LH+10 vs. LH+7 differential genes" = "data/targets/sig_set72.tsv",
+            "All READY 72 targets" = "data/targets/READY_72targets.tsv",
+            "All READY 61 targets" = "data/targets/READY_61targets.tsv"
+          )
+        ),
+        h3("Scoring results"),
+        dataTableOutput("scores_table"),
+        uiOutput("download_score"),
+        h3("PCA of filtered set"),
+        plotlyOutput("pca_filt", width = "auto", height = "auto"),
       )
     )
   )
 )
+
+#### Server
 
 server <- function(input, output) {
 
@@ -229,7 +263,7 @@ server <- function(input, output) {
   )
 
 
-# download ----------------------------------------------------------------
+# download normalized counts --------------------------------------------------
 
   output$download <- renderUI({
     req(norm_biomarkers())
@@ -240,6 +274,229 @@ server <- function(input, output) {
     filename = "TAC-seq_normalized_counts.tsv",
     content = function(file) {
       norm_biomarkers() %>%
+        write_tsv(file)
+    }
+  )
+
+
+# gene subset list --------------------------------------------------------
+
+  gene_subset <- reactive({
+    req(input$gene_subset, norm_biomarkers())
+
+    biomarkers <-
+      norm_biomarkers() %>%
+      select(where(is.numeric)) %>%
+      colnames
+
+    read_tsv(input$gene_subset) %>%
+      filter(target %in% biomarkers)
+  })
+
+
+# scoring control set -----------------------------------------------------
+
+  scoring_ref <- reactive({
+    req(counts(), input$scoring_ref)
+    read_tsv(input$scoring_ref)
+  })
+
+
+# sample gene subsetter ---------------------------------------------------
+
+  filt_biomarkers <- reactive({
+    req(norm_biomarkers(), gene_subset())
+
+    filter_list <-
+      gene_subset() %>% 
+      filter(target %in% colnames(norm_biomarkers())) %>%
+      .$target
+     
+    norm_biomarkers() %>%
+      select(c("sample", filter_list))
+  })
+
+
+# scored counts -----------------------------------------------------------
+
+  mah.dist <- function(x, centr, covar)
+    sweep(as.matrix(x), 2L, centr, "-") %>% 
+      { setNames(rowSums(. %*% solve(covar) * .), rownames(x)) }
+  
+  mah_dist_p <- function(sample_dat, centroids, covs, dgf)
+    foreach(centr = centroids$group, .combine = "full_join") %do% {
+      sample_dat %>%
+        mutate("mah.dist_{centr}" := 
+                 mah.dist(sample_dat %>% select(matches("PC[0-9]+$")),
+                          centroids[centroids$group == centr %>% as.character, -1] %>% 
+                            as.numeric, 
+                          # check if covars are per group or just general
+                          if (is.list(covs)) covs[[centr]] else covs)) %>%
+        mutate("p.chisq_{centr}" :=
+                 1 - pchisq(get(glue("mah.dist_{centr}")), dgf))
+    }
+
+
+  centrs <- function(comp_proj, 
+                     inner_quant = 0, # don't filter IQR values
+                     label_predict = "")
+    comp_proj %>%  # calculate the centroids per reference groups >(label_one, label_two)
+      filter(group != label_predict) %>%
+      select(c(group, where(is.numeric))) %>%
+      group_by(group) %>%
+      # centroids from the inner quantile values of the dataset
+      summarize_all( ~ mean(.x[.x >= quantile(.x, inner_quant) & 
+                            .x <= quantile(.x, 1 - inner_quant)]))
+
+
+  clust_dist <- function(comp_proj, 
+                         label_predict,
+                         n_dim = 3, 
+                         inner_quant = 0) {
+    comp_proj <-
+      comp_proj %>% 
+      # select the n_dim principal components
+      select(-num_range("PC", (n_dim + 1):999))
+  
+    covs <- 
+      comp_proj %>% # covariance matrix over the whole reference PCA
+        filter(!(group %in% c(label_predict))) %>%
+        select(where(is.numeric)) %>%
+        cov
+
+    comp_proj %>%
+      filter(group %in% c(label_predict)) %>%
+      mah_dist_p(centrs(comp_proj, label_predict = label_predict), 
+                 covs, n_dim)
+  }
+
+
+  project_pca <- function(ref_set, 
+                          sample_set, 
+                          ref_groups_list = "all") {
+    ref_set <-
+      ref_set %>%
+      filter(group %in%  # filter out groups if needed
+             if (ref_groups_list != "all") c(ref_groups_list) 
+             else ref_set$group %>% unique)
+
+    ref_set %>%
+      recipe() %>%
+      step_normalize(all_numeric()) %>%
+      step_pca(all_numeric()) %>%
+      prep(strings_as_factors = F) %>% 
+      bake(new_data = 
+           sample_set %>%
+             mutate(group = NA) %>%
+             bind_rows(ref_set)) %>%
+      mutate(group = group %>% as.character) %>%
+      mutate(group = ifelse(is.na(group), "Input", group))
+  }
+
+
+  # TODO: put counts filtering to another object, 
+  # as it is actually not needed in scoring functions
+  scores <- reactive({
+
+    # prepare reference set 
+    sig_genes <- gene_subset()
+    ref_set <- 
+      scoring_ref() %>%
+      select(c("sample", "group", 
+               sig_genes$target %>% as.character)) %>%
+      mutate(group = recode(group, 
+                            "prolif" = "pre", 
+                            "PE" = "pre", 
+                            "LH+2" = "pre", 
+                            "pre-receptive" = "pre",
+                            "LH+7" = "rec",
+                            "receptive" = "rec",
+                            "receptive HRT" = "rec",
+                            "post-receptive" = "post",
+                            "LH+10" = "post"))
+
+    # generate the projected PCA objects for all groups, pre and post comparisons
+    proj_pca_all <-
+      project_pca(ref_set,
+                  filt_biomarkers())
+    proj_pca_pre <-
+      project_pca(ref_set,
+                  filt_biomarkers(),
+                  ref_groups_list = c("pre", "rec"))
+    proj_pca_post <- 
+      project_pca(ref_set,
+                  filt_biomarkers(),
+                  ref_groups_list = c("post", "rec"))
+
+    # calculate the mah. distances, p-values and normalize
+    dists_all <-
+      clust_dist(proj_pca_all, "Input") %>%
+      mutate(norm_pre =  `p.chisq_pre`  / (`p.chisq_rec` + `p.chisq_pre`)) %>%
+      mutate(norm_post = `p.chisq_post` / (`p.chisq_rec` + `p.chisq_post`)) %>%
+      mutate(norm_rec =  `p.chisq_rec`  / (`p.chisq_rec` + `p.chisq_pre`))
+    dists_pre <-
+      clust_dist(proj_pca_pre, "Input") %>%
+      mutate(norm_pre =  `p.chisq_pre`  / (`p.chisq_rec` + `p.chisq_pre`)) %>%
+      mutate(norm_rec =  `p.chisq_rec`  / (`p.chisq_rec` + `p.chisq_pre`))
+    dists_post <-
+      clust_dist(proj_pca_post, "Input") %>%
+      mutate(norm_post = `p.chisq_post` / (`p.chisq_rec` + `p.chisq_post`)) %>%
+      mutate(norm_rec =  `p.chisq_rec`  / (`p.chisq_rec` + `p.chisq_post`))
+
+    sigif <- 0.05
+
+    dists_all %>%
+      transmute(
+        `Sample Name` = sample,
+        `Pre-receptive (%)` = 
+          ifelse(norm_pre >= norm_post & `p.chisq_pre` > sigif & !is.na(norm_pre),
+                 round( dists_pre$norm_pre * 100, 2), NA_real_),
+        `Receptive (%)` = 
+          case_when(`p.chisq_pre` < sigif & `p.chisq_rec` < sigif & `p.chisq_post` < sigif ~ NA_real_,
+                    is.na(norm_pre) & is.na(norm_rec) & is.na(norm_post) ~ NA_real_,
+                    norm_pre >= norm_post ~ round( dists_pre$norm_rec * 100, 2),
+                    T ~ round( dists_post$norm_rec * 100, 2)),
+        `Post-receptive (%)` = 
+          ifelse(norm_pre < norm_post & `p.chisq_post` > sigif & !is.na(norm_post),
+                 round( dists_post$norm_post * 100, 2), NA_real_)
+        ) %>%
+      mutate(
+        `Predicted Group` =
+          case_when(
+            is.na(`Receptive (%)`) ~ "No result",
+            !is.na(`Pre-receptive (%)`) & `Receptive (%)` < 25 ~ "Pre-receptive",
+            !is.na(`Pre-receptive (%)`) & `Receptive (%)` < 50 ~ "Early-receptive",
+            !is.na(`Post-receptive (%)`) & `Receptive (%)` < 25 ~ "Post-receptive",
+            !is.na(`Post-receptive (%)`) & `Receptive (%)` < 50 ~ "Late-receptive",
+            T ~ "Receptive"
+          ),
+
+        `Receptometer score` = case_when(
+          is.na(`Receptive (%)`) ~ NA_real_,
+          is.na(`Post-receptive (%)`) ~ 
+            round( 100 - (`Pre-receptive (%)` / (`Pre-receptive (%)` * 2.4 + `Receptive (%)`)) * 100),
+          is.na(`Pre-receptive (%)`) ~ 
+            round( 200 - (`Post-receptive (%)` / (`Post-receptive (%)` * 2.4 + `Receptive (%)`)) * 100))
+        )
+  })
+
+  output$scores_table <- renderDataTable(
+    scores(),
+    options = list(scrollX = TRUE)
+  )
+
+
+# download scores --------------------------------------------------
+
+  output$download_score <- renderUI({
+    req(scores())
+    downloadButton("score_file", "Download scored results")
+  })
+
+  output$score_file <- downloadHandler(
+    filename = "TAC-seq_scores.tsv",
+    content = function(file) {
+      scores() %>%
         write_tsv(file)
     }
   )
@@ -339,36 +596,101 @@ server <- function(input, output) {
   )
 
 
+# Filtered PCA ---------------------------------------------------------------------
+
+  output$pca_filt <- renderPlotly({
+    sig_genes <- gene_subset()
+    ref_set <- 
+      scoring_ref() %>%
+      select(c("sample", "group", 
+               sig_genes$target %>% as.character)) %>%
+      mutate(group = recode(group, 
+                            "prolif" = "pre", 
+                            "PE" = "pre", 
+                            "LH+2" = "pre", 
+                            "pre-receptive" = "pre",
+                            "LH+7" = "rec",
+                            "receptive" = "rec",
+                            "receptive HRT" = "rec",
+                            "post-receptive" = "post",
+                            "LH+10" = "post"))
+
+    ref_pca <-
+      project_pca(ref_set,
+                  filt_biomarkers())
+
+    plot_ly() %>%
+      add_trace(data = ref_pca %>%
+                  filter(group != "Input"),
+                x = ~PC1, y = ~PC2, z = ~PC3,
+                color = ~group, text = ~sample,
+                marker = list(size = 5), 
+                type = "scatter3d", mode = "markers") %>%
+      add_trace(data = ref_pca %>%
+                  filter(group == "Input"),
+                x = ~PC1, y = ~PC2, z = ~PC3,
+                color = ~group, text = ~sample,
+                marker = list(size = 3, symbol = "x"), 
+                type = "scatter3d", mode = "markers")
+  })
+
+
 # PCA ---------------------------------------------------------------------
 
   output$pca <- renderPlotly({
-    train_data() %>%
+    ref_pca <-
+      train_data() %>%
       recipe() %>%
       step_normalize(all_numeric()) %>%
-      step_pca(all_numeric(), num_comp = 2) %>%
-      prep(strings_as_factors = FALSE) %>%
+      step_pca(all_numeric(), num_comp = 3) %>%
+      prep(strings_as_factors = FALSE) %>% 
       bake(new_data = test_data()) %>%
-      ggplot(aes(PC1, PC2, color = group, shape = group, sample = sample)) +
-      geom_point() +
-      scale_shape_manual(values = 1:7)
-    ggplotly()
+      mutate(group = group %>% as.character) %>%
+      mutate(group = ifelse(is.na(group), "Input", group))
+
+    plot_ly() %>%
+      add_trace(data = ref_pca %>%
+                  filter(group != "Input"),
+                x = ~PC1, y = ~PC2, z = ~PC3,
+                color = ~group, text = ~sample,
+                marker = list(size = 5), 
+                type = "scatter3d", mode = "markers") %>%
+      add_trace(data = ref_pca %>%
+                  filter(group == "Input"),
+                x = ~PC1, y = ~PC2, z = ~PC3,
+                color = ~group, text = ~sample,
+                marker = list(size = 3, symbol = "x"), 
+                type = "scatter3d", mode = "markers")
   })
 
 
 # UMAP --------------------------------------------------------------------
 
   output$umap <- renderPlotly({
-    train_data() %>%
+    ref_umap <-
+      train_data() %>%
       recipe() %>%
       step_normalize(all_numeric()) %>%
       step_string2factor(group) %>%
       step_umap(all_numeric(), outcome = vars(group), seed = c(1, 1)) %>%
       prep(strings_as_factors = FALSE) %>%
       bake(new_data = test_data()) %>%
-      ggplot(aes(umap_1, umap_2, color = group, shape = group, sample = sample)) +
-      geom_point() +
-      scale_shape_manual(values = 1:7)
-    ggplotly()
+      mutate(group = group %>% as.character) %>%
+      mutate(group = ifelse(is.na(group), "Input", group))
+
+    plot_ly() %>%
+      add_trace(data = ref_umap %>% # reference samples
+                  filter(group != "Input"),
+                x = ~umap_1, y = ~umap_2,
+                color = ~group, text = ~sample,
+                marker = list(size = 7), 
+                type = "scatter", mode = "markers") %>%
+      add_trace(data = ref_umap %>% # input samples
+                  filter(group == "Input"),
+                x = ~umap_1, y = ~umap_2,
+                color = ~group, text = ~sample,
+                marker = list(size = 7, symbol = "x"), 
+                type = "scatter", mode = "markers")
   })
 }
 
